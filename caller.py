a874fb5bfc08b4232c412d889c10f55330e9f9a7
Brittney Coble <bwilcutt#gmail.com>
2021-07-07 23:49:33 -0400

    fixed formatting and spelling

--- caller.py
+++ caller.py
@@ -1,16 +1,16 @@
 import logging
-import configparser
 
 
-#todo these make great todo comments as reminders
+# todo these make great todo comments as reminders
 
 # this file is a 'caller' file that calls the other files. it has the logging set up in this file's main function and
 # the logger here can be reused for the other files since they are being called by this file's "main' file and it
-# will recognize logger. just set logger as a param in other file's functions or classes and set it to someting in the function,
-# like a logger.info
+# will recognize logger. just set logger as a param in other file's functions or classes and set it to something in
+# the function, like a logger.info
 
 def main(logger):
-    logger.info(f'------------------------------------process with caller started--------------------------------------')
+    logger.info(
+        f'------------------------------------process with caller started--------------------------------------')
 
     import bc_web_scraper
 
@@ -23,15 +23,16 @@ def main(logger):
 
     logger.info(f'------------------------------------process with caller ended--------------------------------------')
 
+
 if __name__ == '__main__':
-    #setting up custom loggin in the console
+    # setting up custom loggin in the console
 
     logger = logging.getLogger(f'Webscraper Logger')
     logger.setLevel(logging.DEBUG)
     logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
-    #formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    # formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     strhndlr = logging.StreamHandler()
-    #strhndlr.setFormatter(formatter)
+    # strhndlr.setFormatter(formatter)
     logger.addHandler(strhndlr)
 
     main(logger)
